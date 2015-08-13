5df81fc4027aba34938c2eeb5d407080081c569e
Matthew Toussain <mtoussain@gmail.com>
2015-08-12 23:24:07 -0500

    Fixed installation bugs for certain Django builds

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@
 #Setup script is configured to run on Kali Linux (tested on version 2.0) but should work on all debian-based systems
 
 import os
+import subprocess
 
 #Dependencies
 
@@ -44,9 +45,28 @@ except:
 
 #Install Subterfuge
 try:
-   os.system("cp -R ../subterfuge /usr/share/subterfuge")
+   os.system("rm -rf /usr/share/subterfuge/")
+   os.system("rm /usr/share/manage.py")
+   
+   mv = subprocess.call(["cp", "-R", "../subterfuge/", "/usr/share/subterfuge/"])
+
+   if mv == 1:
+      print "[!] Critical Error! Subterfuge install error could not stat primary path. Trying secondary"
+      print "..."
+      subprocess.call(["cp", "-R",  "../Subterfuge/", "/usr/share/subterfuge/"])
+      print "[+] Good! Secondary install mechanism succeeded. Installation continuing..."
+
+   elif mv != 0 and mv != 1:
+     print "[!!] Fatal installation error. Install aborted: Could not stat path: /usr/share/subterfuge"
+
    os.system("cp xsubterfuge /bin/subterfuge")
    os.system("chmod +x /bin/subterfuge")
    os.system("cp manage.py /usr/share/")
+
+   print "[-] Cleaning up"
+
+   os.system("rm /usr/share/subterfuge/xsubterfuge")
+   os.system("rm /usr/share/subterfuge/manage.py")
+
 except:
    print "[!] Critical Error! Subterfuge configuration failed"
