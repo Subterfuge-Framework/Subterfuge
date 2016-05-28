fe2a70becabe13d875d08d2efde9b4101e0e5264
0sm0s1z <mtoussain@gmail.com>
2016-05-28 12:24:43 -0500

    Considering webpack integration

---
+++ gui/apps/feedtable.js
@@ -0,0 +1,39 @@
+//Get Installed Feeds
+var FeedTable = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  render: function() {
+    return (
+      <AvailableFeeds data = {this.state.data} />
+    );
+  }
+});
+var AvailableFeeds  = React.createClass({
+  render: function() {
+    var feedNodes = this.props.data.map(function(feed) {
+         return (
+            <div id = "panelform">
+            <span style={{float:'left'}}>{feed.Name}</span>
+            <br /><br />
+            </div>
+         );
+    });
+    return (
+    <b>{feedNodes}</b>
+    );
+  }
+});
